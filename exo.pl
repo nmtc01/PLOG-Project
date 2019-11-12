f14bfc4896c695c1a8e0627b5c6dc5ed4731b330
Inês Alves <up201605335@fe.up.pt>
2019-11-12 12:25:05 +0000

    pieces

--- exo.pl
+++ exo.pl
@@ -45,20 +45,20 @@ prepare_pack(0, Pack, PackOut, PackNumber, PackOutNumber):-
     PackOut = Pack,
     PackOutNumber = PackNumber.
 
-prepare_pack(NPieces, Pack, PackOut, PackNumber, PackNumberOut):-
-    length(Pack, NElements),
+prepare_pack(NPieces, Pieces, PiecesOut, PackNumber, PackNumberOut):-
+    length(Pieces, NElements),
     randomize_number(NElements, Random),
-    nth0(Random, Pack, Element),
+    nth0(Random, Pieces, Element),
     Next is NPieces-1,
     add_element(Element, PackNumber, NewPackNumber),
-    delete(Pack, Element, NewPack),
-    prepare_pack(Next, NewPack, PackOut, NewPackNumber, PackNumberOut).
+    delete(Pieces, Element, NewPieces),
+    prepare_pack(Next, NewPieces, PiecesOut, NewPackNumber, PackNumberOut).
 
 init_pieces(Pack1, Pack2, Pack3, PackOut1, PackOut2, PackOut3):-
-    Pack = ['g1x', 'r1y', 'b1x', 'r1x', 'b3x', 'r1z', 'b1z', 'g2z', 'b1y', 'g2x', 'b2x', 'r2x', 'b3y', 'b2z', 'b2y', 'r2z', 'g1z', 'r2y', 'g1y', 'g2y', 'r3y', 'b3z', 'r3x', 'g3x', 'g3y', 'g3z', 'r3z'],
-    prepare_pack(9, Pack, NewPack1, Pack1, PackOut1),
-    prepare_pack(9, NewPack1, NewPack2, Pack2, PackOut2),
-    prepare_pack(9, NewPack2, _, Pack3, PackOut3).
+    Pieces = ['g1x', 'r1y', 'b1x', 'r1x', 'b3x', 'r1z', 'b1z', 'g2z', 'b1y', 'g2x', 'b2x', 'r2x', 'b3y', 'b2z', 'b2y', 'r2z', 'g1z', 'r2y', 'g1y', 'g2y', 'r3y', 'b3z', 'r3x', 'g3x', 'g3y', 'g3z', 'r3z'],
+    prepare_pack(9, Pieces, NewPieces1, Pack1, PackOut1),
+    prepare_pack(9, NewPieces1, NewPieces2, Pack2, PackOut2),
+    prepare_pack(9, NewPieces2, _, Pack3, PackOut3).
 
 init_game(Board1, Board2, Score, Pack1, Pack2, Pack3, PackOut1, PackOut2, PackOut3, Player):-
     (Player = 1; Player = 2),
