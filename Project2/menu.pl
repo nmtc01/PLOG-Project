08b8cb5df017b69b313d1e174d30854c42fd7a14
Nuno <up201706162@fe.up.pt>
2020-01-03 20:05:22 +0000

    Option 2 finished

---
+++ Project2/menu.pl
@@ -0,0 +1,50 @@
+:-[display].
+:-[generate_puzzles].
+
+/*
+* Creates a menu with two options
+* 1- Generate random puzzle and solve it
+* 2- Input a puzzle and solve it
+*/
+mainMenu(N, RulesLines, RulesColumns):-
+    displayMainMenu,
+    repeat,
+    read_line(Line),
+    nth0(0, Line, ChoiceCode),
+    char_code(Choice, ChoiceCode),
+    ((Choice = '1', generatePuzzle(N, RulesLines, RulesColumns));
+     (Choice = '2', subMenu(N, RulesLines, RulesColumns))),!.
+
+/*
+* Creates a sub menu - user has to input the board so that the program can solve it
+*/
+subMenu(N, RulesLines, RulesColumns):-
+    repeat,
+    write('Input the number of lines/columns (example: 5):'),nl,
+    write('Number bigger than 4:'),nl,
+    read_line(NCodes),
+    isNumber(NCodes),
+    number_codes(N, NCodes),
+    N > 4,!,
+
+    repeat,
+    nl,write('Input a line rules list with size N:'),nl,
+    write('1 - Represents a star'),nl,
+    write('2 - Represents a white circle'),nl,
+    write('3 - Represents a black'),nl,
+    write('4 - Represents an empty place'),nl,
+    write('Example for size of 5: [1, 2, 3, 4, 4]'),nl,
+    read_line(RLCodes),
+    codesToList(RulesLines, RLCodes, []),
+    length(RulesLines, N),!,
+
+    repeat,
+    nl,write('Input a column rules list with size N:'),nl,
+    write('1 - Represents a star'),nl,
+    write('2 - Represents a white circle'),nl,
+    write('3 - Represents a black'),nl,
+    write('4 - Represents an empty place'),nl,
+    write('Example for size of 5: [1, 2, 3, 4, 4]'),nl,
+    read_line(RCCodes),
+    codesToList(RulesColumns, RCCodes, []),
+    length(RulesColumns, N),!.
--- Project2/starry.pl
+++ Project2/starry.pl
@@ -1,13 +1,9 @@
 :-use_module(library(clpfd)).
-:-[generate_puzzles].
 :-[constraints].
-:-[display].
-
-%Generate puzzle
-    %generate_puzzle(N, RulesLines, RulesColumns),
+:-[menu].
 
 /*
-* Main predicate - start
+* Start predicate
 */
 play:-
     %Start menu
@@ -16,55 +12,7 @@ play:-
     starry(N, _, RulesLines, RulesColumns).
 
 /*
-* Creates a menu with two options
-* 1- Generate random puzzle and solve it
-* 2- Input a puzzle and solve it
-*/
-mainMenu(N, RulesLines, RulesColumns):-
-    displayMainMenu,
-    repeat,
-    read_line(Line),
-    nth0(0, Line, ChoiceCode),
-    char_code(Choice, ChoiceCode),
-    ((Choice = '1', generatePuzzle(N, RulesLines, RulesColumns));
-     (Choice = '2', subMenu(N, RulesLines, RulesColumns))),!.
-
-/*
-* Creates a sub menu - user has to input the board so that the program can solve it
-*/
-subMenu(N, RulesLines, RulesColumns):-
-    repeat,
-    write('Input the number of lines/columns (example: 5):'),nl,
-    write('Number bigger than 4:'),nl,
-    read_line(NCodes),
-    isNumber(NCodes),
-    number_codes(N, NCodes),
-    N > 4,!,
-
-    repeat,
-    nl,write('Input a line rules list with size N:'),nl,
-    write('1 - Represents a star'),nl,
-    write('2 - Represents a white circle'),nl,
-    write('3 - Represents a black'),nl,
-    write('4 - Represents an empty place'),nl,
-    write('Example for size of 5: [1, 2, 3, 4, 4]'),nl,
-    read_line(RLCodes),
-    codesToList(RulesLines, RLCodes, []),
-    length(RulesLines, N),!,
-
-    repeat,
-    nl,write('Input a column rules list with size N:'),nl,
-    write('1 - Represents a star'),nl,
-    write('2 - Represents a white circle'),nl,
-    write('3 - Represents a black'),nl,
-    write('4 - Represents an empty place'),nl,
-    write('Example for size of 5: [1, 2, 3, 4, 4]'),nl,
-    read_line(RCCodes),
-    codesToList(RulesColumns, RCCodes, []),
-    length(RulesColumns, N),!.
-
-/*
-* Solves starry night puzzle
+* Main - Solves starry night puzzle
 */
 starry(N, Vars, RulesLines, RulesColumns):-
     /*
