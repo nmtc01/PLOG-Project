7749d97c2d6bf45c58d060f98bf6403eca3fb087
Nuno <up201706162@fe.up.pt>
2020-01-03 22:58:18 +0000

    Can't figure this out

--- Project2/generate_puzzles.pl
+++ Project2/generate_puzzles.pl
@@ -1,11 +1,12 @@
 :-use_module(library(random)).
 
-generatePuzzle(N, RulesLines, RulesCols):-
+/*generatePuzzle(N, RulesLines, RulesCols):-
     N = 6,
     RulesLines = [1, 4, 4, 4, 2, 4],
-    RulesCols = [4, 3, 2, 4, 1, 1].
+    RulesCols = [4, 3, 2, 4, 1, 1].*/
 
-/*generatePuzzle(N, RulesLines, RulesCols):-
+generatePuzzle(N, RulesLines, RulesCols):-
+    random(5,10, N),
     add_random_to_list(N, [], RulesLines),
     add_random_to_list(N, [], RulesCols).
 
@@ -17,6 +18,6 @@ add_random_to_list(N, List, Out):-
     Next is N-1,
     random(1, 5, RandomNumber),
     append(List, [RandomNumber], Output),
-    add_random_to_list(Next, Output, Out).*/
+    add_random_to_list(Next, Output, Out).
 
     
